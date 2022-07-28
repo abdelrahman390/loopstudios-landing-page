30d9529d885bcb532916546deeba972f3145b65e
abdelrahman390 <88147091+abdelrahman390@users.noreply.github.com>
2022-07-28 10:13:54 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+document.querySelector(".fir-sec header .js-open").onclick = function () {
+ 
+    document.querySelector(".fir-sec header ul").classList.toggle("open");     
+
+}
+
+document.querySelector(".fir-sec header .js-close").onclick = function () {
+ 
+    document.querySelector(".fir-sec header ul").classList.toggle("open");     
+
+}
+
